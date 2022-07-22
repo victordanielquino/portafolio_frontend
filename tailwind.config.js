3bfcd8ebf1f462f937ce945b69d4f833dcc284d8
victor daniel quino jimenez <daniel@victors-MacBook-Pro.local>
2022-07-22 12:10:30 -0400

    [ADD]menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,28 @@
 module.exports = {
   important: true,
   content: ["./src/**/*.{html,ts}"],
+  darkMode: "class",
+  mode: "jit",
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        default: "1rem",
+        sm: "2rem",
+        md: "3rem",
+        lg: "4rem",
+      },
+    },
+    extend: {
+      colors: {
+        primary: {
+          default: "#00ADB5",
+          black: "#222831",
+          gray: "#393E46",
+          white: "#EEEEEE",
+        },
+      },
+    },
   },
   plugins: [],
 };
